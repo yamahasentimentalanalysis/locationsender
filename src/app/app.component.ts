915e4326232774a8756a9affda9d94f4679eb5ca
yamahasentimentalanalysis <hi.sharma2029@gmail.com>
2019-03-11 13:05:40 +0530

    updates to deploy to heroku 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { interval, Subject, observable } from 'rxjs'
 import { PubNubAngular } from 'pubnub-angular2';
 import { DeviceDetectorService } from 'ngx-device-detector';
+import { UUID } from 'angular2-uuid';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,17 +12,21 @@ export class AppComponent {
   sendLocation: any;
   pubnubb: any;
   Data: any;
-  constructor(pubnub: PubNubAngular,
-    private deviceService: DeviceDetectorService) { 
+  keyName:string='ClientKey';
+  constructor(pubnub: PubNubAngular,    
+    private deviceService: DeviceDetectorService) {
     pubnub.init({
       publishKey: 'pub-c-28765844-5b7e-4183-824a-c534d918f3de',
-      subscribeKey: 'sub-c-80a91b4c-43bd-11e9-b827-4e8ff5d9951b' });
+      subscribeKey: 'sub-c-80a91b4c-43bd-11e9-b827-4e8ff5d9951b',
+      uuid: this.setgetLocalStorageByKey(this.keyName)
+    });
 
     this.sendLocation = interval(10000);
     this.sendLocation = this.sendLocation.subscribe(() => {
       if ('geolocation' in navigator) {
         navigator.geolocation.getCurrentPosition((position) => {
           this.Data = {
+            Id:this.setgetLocalStorageByKey(this.keyName),
             Position: {
               accuracy: position.coords.accuracy,
               altitude: position.coords.altitude,
@@ -31,7 +36,8 @@ export class AppComponent {
               longitude: position.coords.longitude,
               speed: position.coords.speed
             },
-            Device: this.getDeviceInfo()
+            Device: this.getDeviceInfo(),
+            DeviceType:this.getDeviceType()
           };
           pubnub.publish({ channel: 'Mobile_Location', message: JSON.stringify(this.Data) }, (response) => {
             console.log(response);
@@ -45,7 +51,24 @@ export class AppComponent {
 
   }
 
-  getDeviceInfo() {
+  getDeviceInfo() {    
     return this.deviceService.getDeviceInfo();
+  }
+  getDeviceType(){
+    return { 
+    Mobile:this.deviceService.isMobile(),
+    Tablet:this.deviceService.isTablet(),
+    Desktop: this.deviceService.isDesktop()
+    }
+  }
+
+  setgetLocalStorageByKey(key: string) {
+    if (!!!localStorage.getItem(key)) {
+      var guid = UUID.UUID();
+      localStorage.setItem(key, guid);
+      return guid;
+    }
+    return localStorage.getItem(key);
+    ;
   }
 }
